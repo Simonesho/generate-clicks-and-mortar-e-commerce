3e7d2f76b15676ede7d5fdef98a22552e2e138ed
Simone <144659113+Simonesho@users.noreply.github.com>
2024-01-16 17:24:18 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+}
+
+
+    }
+            System.out.println("Random Number: " + item);
