e267b02c1a427b434118901b4d841ae6b1e8b2f3
Simone <144659113+Simonesho@users.noreply.github.com>
2024-01-16 18:55:24 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,17 @@
-public class RandomDataGenerator {
-    public static void main(String[] args) {
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-        for (int item : data) {
-        for (int i = 0; i < 10; i++) {
-        int[] data = new int[10];
 }
+        data.push(Math.floor(Math.random() * 100) + 1);
 
 
+    });
+function main() {
+    return data;
+    data.forEach(item => {
+function generateRandomData() {
+}
+main();
     }
-            System.out.println("Random Number: " + item);
+
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+        console.log("Random Number: " + item);
